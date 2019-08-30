6fd3b338ba9630481b4e992b44052844924f4ff9
TomRangren <54435262+TomRangren@users.noreply.github.com>
2019-08-30 09:04:26 +0200

    Create uppg2.cs

---
+++ Lektion 3/uppg2.cs
@@ -0,0 +1,14 @@
+using System;
+
+class MainClass {
+ static void Main()
+{
+  Console.WriteLine("Är det fint väder?: ");
+  string svar = Console.ReadLine();
+  if (svar == "ja" || svar == "Ja")
+  Console.WriteLine("Vi går på picknick!");
+  else if (svar == "nej" || svar == "Nej")
+  Console.WriteLine("Vi stannar inne och läser en bok");
+  
+  }
+}
