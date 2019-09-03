2e76e7f5f74dfb36732f82535a64e0bfd39a1928
TomRangren <54435262+TomRangren@users.noreply.github.com>
2019-09-03 13:32:58 +0200

    Create 0-20 i++ while loop till for loop.cs

---
+++ Lektion 4/0-20 i++ while loop till for loop.cs
@@ -0,0 +1,15 @@
+using System;
+
+class MainClass
+{
+    static void Main()
+    {
+        int i = 0;
+        for ( int I=i; I < 21; I++)
+      
+        {
+         
+            Console.WriteLine(I);
+        }
+    }
+}
