dfa91f914320cb0dac5b85c86ca8f41e3a141052
TomRangren <54435262+TomRangren@users.noreply.github.com>
2019-09-03 13:50:35 +0200

    Create Måste fortsätta.cs

---
+++ Lektion 4/Måste fortsätta.cs
@@ -0,0 +1,19 @@
+using System;
+
+class MainClass
+{
+    public static void Main()
+    {
+        double number1, number2, number3, number4;
+        Console.Write)("Enter the first number: ");
+        number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write)("Enter the second number: ");
+        number2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write)("Enter the third number: ");
+        number3 = Convert.ToDouble(Console.ReadLine());
+        Console.Write)("Enter the fourth number: ");
+        number4 = Convert.ToDouble(Console.ReadLine());
+
+        double result = (number1 + number2 + number3 + number4) / 4;
+        Console.WriteLine("")
